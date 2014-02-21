bea9b09403f39b8ca76460f296bc46a59effd174
unknown <l00095752@LYLIB2-GP26.lyitnet.lyit.ie>
2014-02-21 13:20:06 +0000

    Added eclipse project

---
+++ src/ie/lyit/library/Book.java
@@ -0,0 +1,200 @@
+package ie.lyit.library;
+
+/**
+ * Class for initializing a new Book object.
+ * @author Sean Morris - L00095752
+ *
+ */
+public class Book {
+
+	private int ISBN;
+	private String author;
+	private String title;
+	private int publishedYear;
+	private String genre;
+	private String description;
+	private String publisher;
+	int quantity;
+	
+	/**
+	 * Constructor for instantiating a Book object. (Description not included)
+	 * @param ISBN Book's unique ISBN number.
+	 * @param author Book's author.
+	 * @param title Book's title.
+	 * @param publishedYear Year book was published.
+	 * @param genre Book's Genre.
+	 * @param publisher Book's publisher.
+	 */
+	public Book(int ISBN, String author, String title, int publishedYear, String genre, String publisher) {
+		this.ISBN = ISBN;
+		this.author = author;
+		this.title = title;
+		this.publishedYear = publishedYear;
+		this.genre = genre;
+		this.publisher = publisher;
+		this.quantity = 1;
+	}
+	
+	/**
+	 * Constructor for instantiating a Book object. (Description included)
+	 * @param ISBN Book's unique ISBN number.
+	 * @param author Book's author.
+	 * @param title Book's title.
+	 * @param publishedYear Year book was published.
+	 * @param genre Book's Genre.
+	 * @param publisher Book's publisher.
+	 */
+	public Book(int ISBN, String author, String title, int publishedYear, String genre, String description, String publisher) {
+		this.ISBN = ISBN;
+		this.author = author;
+		this.title = title;
+		this.publishedYear = publishedYear;
+		this.genre = genre;
+		this.description = description;
+		this.publisher = publisher;
+		this.quantity = 1;
+	}
+	
+	/**
+	 * Retrieve the book's unique ISBN.
+	 * @return The book's ISBN.
+	 */
+	public int getISBN() {
+		return ISBN;
+	}
+
+	/**
+	 * Set the book's ISBN.
+	 * @param iSBN The book's unique ISBN number.
+	 */
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+
+	/**
+	 * Retrieve the book's author.
+	 * @return The name of the book's author.
+	 */
+	public String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * Set the author.
+	 * @param author The author's name.
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/**
+	 * Retrieve the book's title.
+	 * @return The book's title.
+	 */
+	public String getTitle() {
+		return title;
+	}
+	
+	/**
+	 * Set the title of the book.
+	 * @param title The book's title.
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+	 * Retrieve the year the book was published in.
+	 * @return The year the book was published.
+	 */
+	public int getPublishedYear() {
+		return publishedYear;
+	}
+
+	/**
+	 * Set the year the book was published.
+	 * @param publishedYear Year the book was published in.
+	 */
+	public void setPublishedYear(int publishedYear) {
+		this.publishedYear = publishedYear;
+	}
+
+	/**
+	 * Retrieve the book's genre.
+	 * @return The book's genre.
+	 */
+	public String getGenre() {
+		return genre;
+	}
+
+	/**
+	 * Set the book's genre.
+	 * @param genre The book's genre.
+	 */
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	/**
+	 * Retrieve the book's description (synopsis).
+	 * @return The book's description.
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Set the description for the book.
+	 * @param description The book's description.
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Retrieve the book's publisher.
+	 * @return The name of the book's publisher.
+	 */
+	public String getPublisher() {
+		return publisher;
+	}
+
+	/**
+	 * Set the book's publisher.
+	 * @param publisher The name of the book's publisher.
+	 */
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	/**
+	 * Retrieve the quantity of books currently available ...
+	 * @return Available quantity.
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * Set the current quantity of books available.
+	 * @param quantity Current quantity of books.
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+	/**
+	 * Return the book's details as a string.
+	 * @return The book's details as a string.
+	 */
+	@Override
+	public String toString() {
+		return "Book [ISBN=" + ISBN + ", author=" + author + ", title=" + title
+				+ ", publishedYear=" + publishedYear + ", genre=" + genre
+				+ ", description=" + description + ", publisher=" + publisher
+				+ ", quantity=" + quantity + "]";
+	}
+
+}
---
+++ src/ie/lyit/library/HomeWindow.java
@@ -0,0 +1,112 @@
+package ie.lyit.library;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.Color;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+/**
+ * The first window seen by the user after launching the application.
+ * @author Sean Morris - l00095752
+ *
+ */
+public class HomeWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField lblSearch;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+		            // Set system look and feel ...
+					UIManager.setLookAndFeel(
+	        		UIManager.getSystemLookAndFeelClassName());
+				} 
+				
+				catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				
+				try {
+					HomeWindow frame = new HomeWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomeWindow() {
+		setResizable(false);
+		setTitle("Library");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 399, 553);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHeader = new JLabel("");
+		lblHeader.setIcon(new ImageIcon(HomeWindow.class.getResource("/images/header.png")));
+		lblHeader.setBounds(0, 0, 404, 60);
+		contentPane.add(lblHeader);
+		
+		JPanel pnlSeparator = new JPanel();
+		pnlSeparator.setBackground(new Color(65, 105, 225));
+		pnlSeparator.setBounds(0, 60, 394, 19);
+		contentPane.add(pnlSeparator);
+		
+		JPanel pnlHeader = new JPanel();
+		pnlHeader.setBounds(0, 0, 394, 80);
+		contentPane.add(pnlHeader);
+		
+		JLabel lblBookImage = new JLabel("");
+		lblBookImage.setIcon(new ImageIcon(HomeWindow.class.getResource("/images/placeholder.jpg")));
+		lblBookImage.setBounds(29, 164, 342, 313);
+		contentPane.add(lblBookImage);
+		
+		lblSearch = new JTextField();
+		lblSearch.setBounds(29, 90, 243, 23);
+		contentPane.add(lblSearch);
+		lblSearch.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setBounds(282, 90, 89, 23);
+		contentPane.add(btnSearch);
+		
+		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.setBounds(29, 124, 130, 29);
+		contentPane.add(btnBrowse);
+		
+		JButton btnLogin = new JButton("Log in / Register");
+		btnLogin.setBounds(241, 124, 130, 29);
+		contentPane.add(btnLogin);
+		
+		JButton btnViewBook = new JButton("View Book");
+		btnViewBook.setBounds(29, 488, 130, 29);
+		contentPane.add(btnViewBook);
+		
+		JButton lblBorrow = new JButton("Borrow Book");
+		lblBorrow.setBounds(241, 488, 130, 29);
+		contentPane.add(lblBorrow);
+	}
+}
