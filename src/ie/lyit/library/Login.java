e9894ca96c93a3049c9f6d6a4e58829643829515
unknown <l00095752@PR2290-08.lyitnet.lyit.ie>
2014-02-28 11:21:46 +0000

    Fixed errors in login screen

--- src/ie/lyit/library/Login.java
+++ src/ie/lyit/library/Login.java
@@ -39,7 +39,7 @@ public class Login extends JFrame {
 	public Login() {
 		setResizable(false);
 		setTitle("Login/Register");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 350, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
