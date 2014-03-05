18f7e24648fb8c02c0d294be41e80023e3778237
unknown <l00095752@PR2401-07.lyitnet.lyit.ie>
2014-03-05 11:05:47 +0000

    Added OverdueLoans.java

--- src/ie/lyit/library/OverdueLoans.java
+++ src/ie/lyit/library/OverdueLoans.java
@@ -1,80 +1,166 @@
 package ie.lyit.library;
 
 import java.awt.Color;
+
 import java.awt.EventQueue;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JCheckBox;
 
-public class OverdueLoans extends JFrame{
-	
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+
+import javax.swing.JOptionPane;
+
+import javax.swing.JPanel;
+
+import javax.swing.JTextField;
+
+import javax.swing.UIManager;
+
+import javax.swing.border.EmptyBorder;
+
+import javax.swing.JTable;
+
+import java.awt.Font;
+
+import java.awt.SystemColor;
+
+public class OverdueLoans extends JFrame {
+
+	private JPanel contentPane;
+
+	private JTable table;
+
+	/**
+	 * 
+	 * Launch the application.
+	 */
+
 	public static void main(String[] args) {
+
 		EventQueue.invokeLater(new Runnable() {
+
 			public void run() {
+
 				try {
-		            // Set system look and feel ...
+
+					// Set system look and feel ...
+
 					UIManager.setLookAndFeel(
-	        		UIManager.getSystemLookAndFeelClassName());
-				} 
-				
+
+					UIManager.getSystemLookAndFeelClassName());
+
+				}
+
 				catch (Exception e) {
+
 					System.out.println(e.getMessage());
+
 				}
-				
+
 				try {
-					OverdueLoans frame = new OverdueLoans();
+
+					HomeWindow frame = new HomeWindow();
+
 					frame.setVisible(true);
+
 				} catch (Exception e) {
+
 					e.printStackTrace();
+
 				}
-				
+
 			}
+
 		});
+
 	}
-	
+
+	/**
+	 * 
+	 * Create the frame.
+	 */
+
 	public OverdueLoans() {
+
 		setResizable(false);
+
 		setTitle("Library");
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 399, 553);
-		JPanel contentPane = new JPanel();
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setBounds(100, 100, 459, 460);
+
+		contentPane = new JPanel();
+
 		contentPane.setBackground(Color.WHITE);
+
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
 		setContentPane(contentPane);
+
 		contentPane.setLayout(null);
-		
+
 		JLabel lblHeader = new JLabel("");
-		lblHeader.setIcon(new ImageIcon(MemberScreen.class.getResource("/images/header.png")));
-		lblHeader.setBounds(0, 0, 404, 60);
+
+		lblHeader.setIcon(new ImageIcon(HomeWindow.class
+				.getResource("/images/header.png")));
+
+		lblHeader.setBounds(0, 0, 453, 60);
+
 		contentPane.add(lblHeader);
-		
+
 		JPanel pnlSeparator = new JPanel();
+
 		pnlSeparator.setBackground(new Color(65, 105, 225));
-		pnlSeparator.setBounds(0, 60, 394, 19);
+
+		pnlSeparator.setBounds(0, 60, 453, 19);
+
 		contentPane.add(pnlSeparator);
-		
+
 		JPanel pnlHeader = new JPanel();
+
 		pnlHeader.setBounds(0, 0, 394, 80);
+
 		contentPane.add(pnlHeader);
-		
-		JButton btnSendNotification = new JButton("Send Notification");
-		btnSendNotification.addActionListener(new ActionListener() {
+
+		JButton btnNotify = new JButton("Send Notification");
+
+		btnNotify.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent arg0) {
+
+				JOptionPane.showInputDialog("Loan Renewed.");
+
 			}
+
 		});
-		btnSendNotification.setBounds(123, 487, 156, 25);
-		contentPane.add(btnSendNotification);
-		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
-		chckbxNewCheckBox.setBounds(79, 141, 129, 23);
-		contentPane.add(chckbxNewCheckBox);
+
+		btnNotify.setBounds(231, 353, 163, 29);
+
+		contentPane.add(btnNotify);
+
+		table = new JTable();
+
+		table.setBackground(Color.LIGHT_GRAY);
+
+		table.setBounds(52, 148, 342, 181);
+
+		contentPane.add(table);
+
+		JButton btnBack = new JButton("Back");
+
+		btnBack.setBounds(52, 355, 163, 26);
+
+		contentPane.add(btnBack);
+
 	}
+
 }
